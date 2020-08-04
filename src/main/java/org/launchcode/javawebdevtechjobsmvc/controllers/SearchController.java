cd1b1bf611a8f92a08110483ad6f6979483aaa7f
jedfarm <jfand72@gmail.com>
2020-08-03 21:59:35 -0400

    Implemented search functionality.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -4,12 +4,13 @@ import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
-
 /**
  * Created by LaunchCode
  */
@@ -24,5 +25,25 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @RequestMapping(value = "results", method={RequestMethod.POST})
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        List<Job> jobs;
+
+        if(searchType.toLowerCase() == "all" && searchTerm == ""){
+            jobs = JobData.findAll();
+
+        } else if (searchType.toLowerCase() == "all"){
+            jobs = JobData.findByValue(searchTerm);
+
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
 
 }
